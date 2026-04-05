87a4405b32bbf404ce8a6a7db7178633a87f9687
dineshyr29-04 <dineshyr2904@gmail.com>
2026-04-05 15:30:25 +0530

    the cors was updated

--- app.js
+++ app.js
@@ -11,24 +11,27 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
-// Middleware
 app.use((req, res, next) => {
-    // Handle Private Network Access (PNA) checks from browsers like Chrome
-    if (req.headers['access-control-request-private-network']) {
-        res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    const origin = req.headers.origin;
+    if (origin) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
     }
+    
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Private-Network');
+    
+    // Handle Private Network Access (PNA) checks from browsers like Chrome
+    res.setHeader('Access-Control-Allow-Private-Network', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     next();
 });
-
-app.use(cors({
-    origin: function(origin, callback) {
-        callback(null, true); // Allow any origin for maximum compatibility with Vercel preview links
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Private-Network'],
-    optionsSuccessStatus: 200
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
